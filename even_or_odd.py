ec9f3981cb4b0d8e8c579fbfb3611ac77bfd0a77
19h61a0519 <69337244+19h61a0519@users.noreply.github.com>
2020-12-15 16:29:25 +0530

    Add files via upload

---
+++ even_or_odd.py
@@ -0,0 +1,6 @@
+#wap to check whether the given number is even or odd
+a=int(input("Enter a number to check if a number is even or odd...\n"))
+if a%2==0:
+    print(str(a)+" is an Even Number")
+else:
+    print(str(a)+" is an Odd Number ")
